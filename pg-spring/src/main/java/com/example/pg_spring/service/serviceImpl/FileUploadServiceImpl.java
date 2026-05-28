ce86a703cef5abcaa4c43e59ea37867c55921d89
Khushbu <Kahlwt08@gmail.com>
2026-05-28 17:59:29 +0530

    backend file upload using Cloudinary

--- pg-spring/src/main/java/com/example/pg_spring/service/serviceImpl/FileUploadServiceImpl.java
+++ pg-spring/src/main/java/com/example/pg_spring/service/serviceImpl/FileUploadServiceImpl.java
@@ -1,5 +1,7 @@
 package com.example.pg_spring.service.serviceImpl;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import com.example.pg_spring.service.FileUploadService;
 import io.minio.*;
 import io.minio.http.Method;
@@ -11,12 +13,15 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 @Service
 public class FileUploadServiceImpl implements FileUploadService {
     private final MinioClient minioClient;
+    private final Cloudinary cloudinary;
+
 
     @Value("${minio.bucket}")
     private String bucketName;
@@ -24,63 +29,100 @@ public class FileUploadServiceImpl implements FileUploadService {
     @Value("${minio.public.url}")
     private String publicUrl;
 
-    public FileUploadServiceImpl(MinioClient minioClient) {
+    public FileUploadServiceImpl(MinioClient minioClient, Cloudinary cloudinary) {
+        this.cloudinary = cloudinary;
         this.minioClient = minioClient;
     }
 
     @Override
     public String uploadFile(MultipartFile file, String folderName) {
         try {
-            String fileName = folderName + "/"
-                            + UUID.randomUUID()
-                            + "_"
-                            + file.getOriginalFilename();
-            InputStream inputStream = file.getInputStream();
-            minioClient.putObject(
-                    PutObjectArgs.builder()
-                            .bucket(bucketName)
-                            .object(fileName)
-                            .stream(inputStream, file.getSize(), -1)
-                            .contentType(file.getContentType())
-                            .build()
-            );
-            // Return browser accessible URL
-            return publicUrl
-                    + "/"
-                    + bucketName
-                    + "/"
-                    + fileName;
+
+            Map uploadResult = cloudinary.uploader().upload(
+                    file.getBytes(), ObjectUtils.asMap(
+                                    "folder", folderName,
+                                    "resource_type", "auto"
+                            )
+                    );
+            return uploadResult.get("secure_url").toString();
         } catch (Exception e) {
             e.printStackTrace();
-            throw new RuntimeException("File upload failed");
+            throw new RuntimeException("File upload failed"
+            );
         }
     }
 
     @Override
     public List<String> getPgImages() {
-        List<String> pgImages = new ArrayList<>();
         try {
-            Iterable<Result<Item>> objects =
-                    minioClient.listObjects(
-                            ListObjectsArgs.builder()
-                                    .bucket(bucketName)
-                                    .prefix("pg-images/")
-                                    .build()
-                    );
-            for (Result<Item> result : objects) {
-                Item item = result.get();
-                String imageUrl =
-                        publicUrl + "/"
-                                + bucketName + "/"
-                                + item.objectName();
-                pgImages.add(imageUrl);
+            Map result = cloudinary.search().expression("folder:pg-images").execute();
+            List<Map> resources = (List<Map>) result.get("resources");
+            List<String> imageUrls = new ArrayList<>();
+            for (Map resource : resources) {
+                imageUrls.add(resource.get("secure_url").toString());
             }
+            return imageUrls;
         } catch (Exception e) {
             e.printStackTrace();
-            throw new RuntimeException(
-                    "Failed to fetch PG images"
-            );
+            throw new RuntimeException("Failed to fetch PG images");
         }
-        return pgImages;
     }
+
+
+//    @Override
+//    public String uploadFile(MultipartFile file, String folderName) {
+//        try {
+//            String fileName = folderName + "/"
+//                            + UUID.randomUUID()
+//                            + "_"
+//                            + file.getOriginalFilename();
+//            InputStream inputStream = file.getInputStream();
+//            minioClient.putObject(
+//                    PutObjectArgs.builder()
+//                            .bucket(bucketName)
+//                            .object(fileName)
+//                            .stream(inputStream, file.getSize(), -1)
+//                            .contentType(file.getContentType())
+//                            .build()
+//            );
+//            // Return browser accessible URL
+//            return publicUrl
+//                    + "/"
+//                    + bucketName
+//                    + "/"
+//                    + fileName;
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            throw new RuntimeException("File upload failed");
+//        }
+//    }
+//
+//    @Override
+//    public List<String> getPgImages() {
+//        List<String> pgImages = new ArrayList<>();
+//        try {
+//            Iterable<Result<Item>> objects =
+//                    minioClient.listObjects(
+//                            ListObjectsArgs.builder()
+//                                    .bucket(bucketName)
+//                                    .prefix("pg-images/")
+//                                    .build()
+//                    );
+//            for (Result<Item> result : objects) {
+//                Item item = result.get();
+//                String imageUrl =
+//                        publicUrl + "/"
+//                                + bucketName + "/"
+//                                + item.objectName();
+//                pgImages.add(imageUrl);
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            throw new RuntimeException(
+//                    "Failed to fetch PG images"
+//            );
+//        }
+//        return pgImages;
+//    }
+
 }
